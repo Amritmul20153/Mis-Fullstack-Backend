e919fdc4b1d53f2ea622009e00374af8d4536d90
amrit <mul20153@spengergasse.at>
2025-12-17 21:46:35 +0100

    Backend für die Übung Erweiterung um FHIR search fertig :)

--- src/test/java/at/spengergasse/spengermed/utilities/config/SecurityConfig.java
+++ src/test/java/at/spengergasse/spengermed/utilities/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package at.spengergasse.spengermed.utilities.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,14 +12,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/api/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()   // ← ALLES offen, auch /api/**
                 )
-                .csrf(csrf -> csrf.disable());
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+                )
+                .cors(cors -> {});             // CORS aktivieren
+
         return http.build();
     }
 }
-
